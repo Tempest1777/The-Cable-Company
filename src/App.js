3314b10a88030cf987c04788b0602c1f843ba999
Sean <seanoinca@gmail.com>
2022-03-14 09:53:20 -0700

    Initial Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard/Dashboard"
+import UpdateCaptar from "./pages/UpdateCaptar/UpdateCaptar"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/Update" element={<UpdateCaptar />} />
       {/* <Route path="expenses" element={<Expenses />} />
       <Route path="invoices" element={<Invoices />} /> */}
     </Routes>
---
+++ src/pages/UpdateCaptar/UpdateCaptar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateCaptar = () => {
+  return <div>UpdateCaptar</div>;
+};
+
+export default UpdateCaptar;
