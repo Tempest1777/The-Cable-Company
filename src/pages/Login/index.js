c6db316a3e85f221a67a2bb3554ab56cd9beaa6f
Libby <lteffera@ucmerced.edu>
2022-03-22 20:54:49 -0700

    Add: skeleton login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./styles.css";
+import slaclogo from '../Login/slaclogo.png';
+import Background from "../../components/Background/background.js";
 
 const Login = () => {
-  return <div>fbfhfbhjfm nbvhjmfdbfvhjkfmfbvhjfkmfnbjfkm</div>;
-};
+ return <div> 
+ 
+ <img src={slaclogo} alt="Logo"
+     width = {120} 
 
+    
+
+ 
+ /> 
+
+  <Background/>
+    
+  </div>;
+  <h1>    we/ldelkjqf;3  </h1>
+  
+};
+ 
 export default Login;
