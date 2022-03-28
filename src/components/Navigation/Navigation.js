6202763596040655e67cecbe07a1f48bcc65c936
Sean <seanoinca@gmail.com>
2022-03-28 09:11:37 -0700

    Navigation Component done

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,25 +1,34 @@
 import React from "react";
 import './Navigation.css';
-
+import Comment from "../../assets/comment.svg"
+import Notification from "../../assets/notification.svg"
 import CompanyLogo from '../../assets/SLAC-simple-hires-removebg-preview.png'
+import {Link} from "react-router-dom";
 function Nav(){
   return <html><head></head><div className="background">
   <img src={CompanyLogo} width="170" className="companyLogo"/>
   <button className="button"><p className="CreateNew">+ Create New</p></button>
   <div className="NavigationList">
+  <div className="userInfo">
+    
+    <div className="iconContainer">
+    <img src={Notification} width="25" className="icon"/>
+    </div>
+    <div className="iconContainer">
+    <p>John Doe</p>
+    </div>
+     </div>
       <h1 className="listTitle">MY DESK</h1>
       <ul>
-          <li className="listText">Dashboard</li>
-          <li className="listText">Sent Items</li>
-          <li className="listText">Cable Listing</li>
-          <li className="listText">Reports</li>
-          <li className="listText">Documents</li>
+      <Link to="/dashboard"><li className="listText Dashboard">Dashboard</li></Link>
+      <Link to="/sentitems"><li className="listText Sent">Sent Items</li></Link>
+      <Link to="/cablelisting"><li className="listText Cable">Cable Listing</li></Link>
+      <Link to="/documents"><li className="listText Documents">Documents</li></Link>
       </ul>
       <h1 className="listTitle">ACCOUNT</h1>
       <ul>
-          <li className="listText">Settings</li>
-          <li className="listText">Support</li>
-          <li className="listText">Logout</li>
+      <Link to="/settings"><li className="listText Settings">Settings</li></Link>
+      <Link to="/logout"><li className="listText Logout">Logout</li></Link>
       </ul>
   </div>
   
