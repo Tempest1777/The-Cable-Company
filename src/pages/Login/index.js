442f329b8c48436185d4945312dab370ba846c2b
Libby <lteffera@ucmerced.edu>
2022-03-29 18:29:35 -0700

    user login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,10 +8,38 @@ const Login = () => {
     <div>
       <img className="Logo" src={slaclogo} alt="Logo" width={120} />
       <div className="basediv">
-        <img className="waves" src={waves} alt="Logo" />
+        <img className="waves" src={waves} alt="Logo" /> 
+        <div className="form">
+      <div className="form-field username">
+        <div className="icon">
+          <i className="far fa-user"></i>
+        </div>
+        <input type="text" placeholder="Username"/>
       </div>
+      <div className="form-field password">
+        <div className="icon">
+          <i className="fas fa-lock"></i>
+        </div>
+        <input type="password" placeholder="Password"/>
+      </div>
+
+      <button className="login-button" type="submit">
+        LOGIN
+      </button>
+      
     </div>
+  </div>
+  <div className="login-card-footer">
+   
+  </div>
+</div>    
+        
+    
+
   );
 };
+
+
+
 
 export default Login;
