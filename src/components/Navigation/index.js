c30a3e71039e7820fe11ab6cc2050da02506d6c5
Mary <mdelosreyes3@ucmerced.edu>
2022-03-29 17:19:37 -0700

    add: navigation icons png

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 import CompanyLogo from "../../assets/SLAC-simple-hires-removebg-preview.png";
+import HomeIcon from "../../assets/home-icon.png";
+import SendIcon from "../../assets/send-icon.png";
+import CableIcon from "../../assets/list-icon.png";
+import DocumentsIcon from "../../assets/file-icon.png";
+import SettingsIcon from "../../assets/settings-icon.png";
+import LogoutIcon from "../../assets/logout-icon.png";
+
 import { Link } from "react-router-dom";
 
 import "./styles.css";
@@ -19,25 +26,31 @@ const Nav = () => {
           </div>
           <h1 className="listTitle">MY DESK</h1>
           <ul>
-            <Link to="/dashboard">
-              <li className="listText Dashboard">Dashboard</li>
+          <img src={HomeIcon} width="20" className="homeIcon" />
+            <Link to="/dashboard" style={{ textDecoration: 'none' }}>
+              <li className="listText Dashboard"> Dashboard</li>
             </Link>
-            <Link to="/sentitems">
+            <img src={SendIcon} width="20" className="sendIcon" />
+            <Link to="/sentitems" style={{ textDecoration: 'none' }}>
               <li className="listText Sent">Sent Items</li>
             </Link>
-            <Link to="/cablelisting">
+            <img src={CableIcon} width="20" className="cableIcon" />
+            <Link to="/cablelisting" style={{ textDecoration: 'none' }}>
               <li className="listText Cable">Cable Listing</li>
             </Link>
-            <Link to="/documents">
+            <img src={DocumentsIcon} width="20" className="documentsIcon" />
+            <Link to="/documents" style={{ textDecoration: 'none' }}>
               <li className="listText Documents">Documents</li>
             </Link>
           </ul>
           <h1 className="listTitle">ACCOUNT</h1>
           <ul>
-            <Link to="/settings">
+          <img src={SettingsIcon} width="20" className="settingsIcon" />
+            <Link to="/settings" style={{ textDecoration: 'none' }}>
               <li className="listText Settings">Settings</li>
             </Link>
-            <Link to="/logout">
+            <img src={LogoutIcon} width="20" className="logoutIcon" />
+            <Link to="/logout" style={{ textDecoration: 'none' }}>
               <li className="listText Logout">Logout</li>
             </Link>
           </ul>
